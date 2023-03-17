d5d55e669ace0021200de5e4c56715ffc7c73dde
Charly Delay <charly@delay.gg>
2023-03-17 18:10:25 +0900

    Fix capabilities setup for nvim lsp

--- .config/nvim/lua/user/plugins/lsp.lua
+++ .config/nvim/lua/user/plugins/lsp.lua
@@ -30,7 +30,9 @@ return {
       -- nvim-cmp supports additional completion capabilities.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require 'cmp_nvim_lsp'.default_capabilities(capabilities)
-      capabilities = require 'cmp_nvim_ciderlsp'.update_capabilities(capabilities)
+      if company.is_corporate_host() then
+        capabilities = require 'cmp_nvim_ciderlsp'.update_capabilities(capabilities)
+      end
       capabilities.textDocument = {
         foldingRange = {
           dynamicRegistration = false,
