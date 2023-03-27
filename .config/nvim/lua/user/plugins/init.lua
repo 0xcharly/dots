590c536624bf940e300f9cc3ba53c5a59b37c1a8
Charly Delay <charly@delay.gg>
2023-03-27 17:08:01 +0900

    Replace tpope/vim-surround with kylechui/nvim-surround

--- .config/nvim/lua/user/plugins/init.lua
+++ .config/nvim/lua/user/plugins/init.lua
@@ -4,7 +4,6 @@ return {
 
   --- Motions and other convenience plugins.
   { 'tpope/vim-repeat' },
-  { 'tpope/vim-surround' },
   {
     'mbbill/undotree',
     keys = {
@@ -12,18 +11,20 @@ return {
     },
   },
   { 'asiryk/auto-hlsearch.nvim', event = 'BufReadPost', config = true },
-  { 'numToStr/Comment.nvim', lazy = false, config = true },
-  { 'ethanholz/nvim-lastplace', lazy = false, config = true },
+  { 'kylechui/nvim-surround',    event = 'VeryLazy',    config = true },
+  { 'numToStr/Comment.nvim',     lazy = false,          config = true },
+  { 'ethanholz/nvim-lastplace',  lazy = false,          config = true },
 
-  { -- Better increment/decrement.
+  {
+    -- Better increment/decrement.
     'monaqa/dial.nvim',
     keys = {
-      { '<C-a>', function() return require('dial.map').inc_normal() end, expr = true, desc = 'Increment' },
-      { '<C-x>', function() return require('dial.map').dec_normal() end, expr = true, desc = 'Decrement' },
+      { '<C-a>', function() return require 'dial.map'.inc_normal() end,  expr = true, desc = 'Increment' },
+      { '<C-x>', function() return require 'dial.map'.dec_normal() end,  expr = true, desc = 'Decrement' },
     },
     config = function()
       local augend = require 'dial.augend'
-      require('dial.config').augends:register_group {
+      require 'dial.config'.augends:register_group {
         default = {
           augend.integer.alias.decimal,
           augend.integer.alias.hex,
@@ -35,14 +36,16 @@ return {
     end,
   },
 
-  { -- Structural Search & Replace.
+  {
+    -- Structural Search & Replace.
     'cshuaimin/ssr.nvim',
     keys = {
-      { '<leader>ssr', function() require('ssr').open() end, mode = { 'n', 'x' }, desc = 'Structural Replace' },
+      { '<leader>ssr', function() require 'ssr'.open() end, mode = { 'n', 'x' }, desc = 'Structural Replace' },
     },
   },
 
-  { -- 3-way diff-view.
+  {
+    -- 3-way diff-view.
     'sindrets/diffview.nvim',
     cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
     config = true,
