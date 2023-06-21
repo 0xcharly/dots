7e3bd71e0e733a63bc88cfa80a1fc8d0d473501c
Charly Delay <charly@delay.gg>
2023-06-21 15:55:35 +0900

    Update scripts

--- .local/bin/kitty-update-icon.sh
+++
@@ -1,4 +0,0 @@
-cp ~/.local/share/kitty/kitty.icns /Applications/kitty.app/Contents/Resources/kitty.icns
-rm /var/folders/*/*/*/com.apple.dock.iconcache
-touch /Applications/kitty.app
-killall Dock && killall Finder
--- .local/bin/set-pip-position.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-i3-msg -t command "[title=\"Picture in picture\"] border none, resize set width 1000 px, move position 4111 px 1557 px"
